b498300e3edfb1a52075118d83710655c6365a54
SeymurR <seymuram@code.edu.az>
2024-04-08 17:37:09 +0400

    add queries

--- src/WebApi/Twitter.Post.Api/Controllers/PostController.cs
+++ src/WebApi/Twitter.Post.Api/Controllers/PostController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Twitter.Post.Application.Interfaces.Repository;
 using MediatR;
 using Twitter.Post.Application;
+using Twitter.Post.Application.Features.Queries;
 
 namespace Twitter.Post.Api.Controllers;
 
@@ -18,6 +19,13 @@ public class PostController : ControllerBase
     
     [HttpPost("CreatePost")]
     public async Task<IActionResult> CreatePost(CreatePostCommand createCommandPost) =>
-       Ok(await _mediator.Send(createCommandPost));      
+       Ok(await _mediator.Send(createCommandPost));     
+    [HttpGet("GetAllPosts")]
+    public async Task<IActionResult> GetAllPosts( ) =>
+       Ok(await _mediator.Send(new GetAllPostQuery { }));
+    [HttpGet("GetDiscount")]
+    public async Task<IActionResult> GetDiscount(string id) =>
+        Ok(await this.mediator.Send(new GetDiscountCommand { id }));   
+
     
 }
